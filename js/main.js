dd3cdd9022abebba9817e3a0a3cf28895de43f3d
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-06 20:27:14 +0900

    練習問題

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+{
+  const input = document.getElementById('input');
+  const total = document.getElementById('total');
+  const tax = document.getElementById('tax');
+
+  input.addEventListener('change', () => {
+    if (input.value !== "") {
+      // 消費税の計算
+      tax.textContent = Math.floor(input.value * 0.1);
+      
+      // 合計金額の計算
+      total.textContent = Math.floor(input.value * 1.1);
+    } else {
+      tax.textContent = null;
+      total.textContent = null;
+    }
+
+
+  })
+}
