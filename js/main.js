5a0938443511e1733b4c2796a7ed13175fe9dc66
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-10 00:16:25 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -1,23 +1,71 @@
 'use strict';
 {
-  const src = ["img/1.png","img/2.png","img/3.png","img/4.png"];
-  let num1 = 0;
-  let num2 = 0;
-  
-  document.getElementById('btn1').addEventListener('click', () => {
-    
-    if (num1 < src.length - 1) {
-      num1 ++;
-      document.getElementById('pic1').src = src[num1];
-    } else {
-      num1 = 0;
-      document.getElementById('pic1').src = src[num1];
-    }
-  })
+  // 1 配列とif文を使った繰り返し処理
+  const list = ['img/1.png', 'img/2.png', 'img/3.png', 'img/4.png'];
+  let num = 0;
 
-  document.getElementById('btn2').addEventListener('click', () => {
-    num2 = Math.floor(Math.random() * src.length);
-    document.getElementById('pic2').src = src[num2];
-  })
+  const pic = document.getElementById('pic');
+
+  pic.addEventListener('mouseover', () => {
+    let id = setInterval(change,1000);
+  
+    function change () {
+      if (num < list.length - 1) {
+        num++;
+        pic.src = list[num]; 
+      } else {
+        num = 0;
+        pic.src = list[num] 
+      }
+    }
+
+    pic.addEventListener('mouseleave', () => {
+      clearTimeout(id);
+    });
+  });
+
+  // 2 オブジェクトとswitch文を使った繰り返し処理
+  const list = {one:'img/1.png',two:'img/2.png',three: 'img/3.png',four:'img/4.png'}
+  let num = 0;
+
+  const pic = document.getElementById('pic');
+  pic.addEventListener('mouseover', () => {
+    let id = setInterval(change,1000);
+
+    function change() {
+      switch (num) {
+        case 0:
+          pic.src = list.one;
+          num++;
+          break; 
+        case 1:
+          pic.src = list.two;
+          num++;
+          break;
+        case 2:
+          pic.src = list.three;
+          num++;
+          break;
+        case 3:
+          pic.src = list.four;
+          num = 0;
+          break;
+      }
+    };
+
+    pic.addEventListener('mouseleave', () => {
+      clearTimeout(id);
+    })
+  });
+
+
+
+
+
+  
+
+
+
+
 
 }
