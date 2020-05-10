43aeafcfdf8b0f23b05f6b513828b284644d6cd1
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-10 21:08:16 +0900

    練習問題 オブジェクト

--- js/main.js
+++ js/main.js
@@ -1,61 +1,24 @@
 'use strict';
 {
-  // 1 配列とif文を使った繰り返し処理
-  const list = ['img/1.png', 'img/2.png', 'img/3.png', 'img/4.png'];
-  let num = 0;
+  let x = 3;
+  let same = 2 ;
 
-  const pic = document.getElementById('pic');
-
-  pic.addEventListener('mouseover', () => {
-    let id = setInterval(change,1000);
-  
-    function change () {
-      if (num < list.length - 1) {
-        num++;
-        pic.src = list[num]; 
-      } else {
-        num = 0;
-        pic.src = list[num] 
-      }
-    }
-
-    pic.addEventListener('mouseleave', () => {
-      clearTimeout(id);
-    });
-  });
-
-  // 2 オブジェクトとswitch文を使った繰り返し処理
-  const list = [
-    {img:'img/1.png'},
-    {img:'img/2.png'},
-    {img:'img/3.png'},
-    {img:'img/4.png'}
-  ];
-  
-  const pic = document.getElementById('pic');
-  let num = 0;
-  
-
-  const  change = () => {
-    num < list.length - 1 ? num++ : num = 0;
-    pic.src = list[num].img
+  let obj = {
+    one: 1,
+    two: 2,
+    three: x,
+    same
   }
-
-  pic.addEventListener('mouseover', () => {
-    let id = setInterval(change,500);
-    
-    pic.addEventListener('mouseleave', () => {
-      clearTimeout(id);
-    })
-  });
-
-
-
-
-
   
+  // 追加
+  obj.four = 4;
 
+  // 削除
+  delete obj.one
 
+  console.log(obj);
+  console.log(obj.same);
+  
 
 
 
