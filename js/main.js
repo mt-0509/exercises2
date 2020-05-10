80213362a848f7aa15193c2f4d7e0b1eb734b2b6
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-10 10:08:40 +0900

    条件演算子

--- js/main.js
+++ js/main.js
@@ -25,34 +25,25 @@
   });
 
   // 2 オブジェクトとswitch文を使った繰り返し処理
-  const list = {one:'img/1.png',two:'img/2.png',three: 'img/3.png',four:'img/4.png'}
-  let num = 0;
-
+  const list = [
+    {img:'img/1.png'},
+    {img:'img/2.png'},
+    {img:'img/3.png'},
+    {img:'img/4.png'}
+  ];
+  
   const pic = document.getElementById('pic');
+  let num = 0;
+  
+
+  const  change = () => {
+    num < list.length - 1 ? num++ : num = 0;
+    pic.src = list[num].img
+  }
+
   pic.addEventListener('mouseover', () => {
-    let id = setInterval(change,1000);
-
-    function change() {
-      switch (num) {
-        case 0:
-          pic.src = list.one;
-          num++;
-          break; 
-        case 1:
-          pic.src = list.two;
-          num++;
-          break;
-        case 2:
-          pic.src = list.three;
-          num++;
-          break;
-        case 3:
-          pic.src = list.four;
-          num = 0;
-          break;
-      }
-    };
-
+    let id = setInterval(change,500);
+    
     pic.addEventListener('mouseleave', () => {
       clearTimeout(id);
     })
