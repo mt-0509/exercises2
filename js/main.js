8144ce226ecbad6135eaed302510b88b7fc961a9
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-12 00:56:52 +0900

    forEachで繰り返し

--- js/main.js
+++ js/main.js
@@ -1,36 +1,16 @@
 'use strict';
 {
-  const img = document.getElementsByTagName('img');
-  const img2 = ['img/1.png','img/2.png','img/3.png','img/4.png','img/5.gif'];
+  const img = document.querySelectorAll('.hover');
+  let apper = document.getElementsByTagName('img');
 
-  img[0].addEventListener('mouseenter', () => {
-    img[4].src = img2[0]
-  });
-  img[0].addEventListener('mouseleave', () => {
-    img[4].src = img2[4]
-  });
+  apper = apper[apper.length - 1];
 
-  img[1].addEventListener('mouseenter', () => {
-    img[4].src = img2[1]
+  img.forEach(el => {
+    el.addEventListener('mouseenter', () => {
+      apper.src = el.src;
+    });
+    el.addEventListener('mouseleave', () => {
+      apper.src = "img/5.gif";
+    })
   });
-  img[1].addEventListener('mouseleave', () => {
-    img[4].src = img2[4]
-  });
-
-  img[2].addEventListener('mouseenter', () => {
-    img[4].src = img2[2]
-  });
-  img[2].addEventListener('mouseleave', () => {
-    img[4].src = img2[4]
-  });
-  
-  img[3].addEventListener('mouseenter', () => {
-    img[4].src = img2[3]
-  });
-  img[3].addEventListener('mouseleave', () => {
-    img[4].src = img2[4]
-  })
-  
-
-
 }
