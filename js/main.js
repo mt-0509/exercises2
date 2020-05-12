05f36c41cb88210fb5e53b1aba298bf720986132
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-12 22:03:52 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
 'use strict';
 {
-  const img = document.querySelectorAll('.hover');
-  let apper = document.getElementsByTagName('img');
+  // ①
+  let check = document.getElementById('check');
+  const checkbox = document.form.check;
+  
+  const btn = document.getElementById('btn');
+  btn.disabled = true;
 
-  apper = apper[apper.length - 1];
-
-  img.forEach(el => {
-    el.addEventListener('mouseenter', () => {
-      apper.src = el.src;
-    });
-    el.addEventListener('mouseleave', () => {
-      apper.src = "img/5.gif";
-    })
+  check.addEventListener('click', () => {
+    if(check.checked) {
+      btn.disabled = false
+      btn.addEventListener('click', () => {
+        alert('送信されました');
+      });
+    } else {
+      btn.disabled = true;
+    }
   });
+
+
+  // ②
+  let check = document.getElementById('check');
+  const checkbox = document.form.check;
+  
+  const btn = document.getElementById('btn');
+  
+  let flag = true;
+  btn.addEventListener('click', () => {
+    if (check.checked) {
+      alert('同意が確認できました');
+
+      // 次に進むボタン作成
+      const input = document.createElement('input');
+      const button = document.createElement('button')
+      input.type = 'button';
+      input.value = '次に進む';
+      if (flag === true) {
+        document.body.appendChild(input);
+        flag = false;
+      };
+
+      input.addEventListener('click', () => {
+        if (check.checked) {
+          alert('次に進みます');
+        } else {
+          alert('同意されていません')
+        }
+      });
+    } else {
+      alert('同意が確認できません');
+    }
+  });
+
+
+
 }
