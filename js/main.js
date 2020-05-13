dbbb997dc26dbcb1ca232d8b9aed078a44832a80
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-13 22:01:50 +0900

    練習問題 ドロップダウンメニュー

--- js/main.js
+++ js/main.js
@@ -1,57 +1,26 @@
 'use strict';
 {
-  // ①
-  let check = document.getElementById('check');
-  const checkbox = document.form.check;
+  let select = document.getElementById('select');
+  const text = document.getElementById('text');
   
-  const btn = document.getElementById('btn');
-  btn.disabled = true;
-
-  check.addEventListener('click', () => {
-    if(check.checked) {
-      btn.disabled = false
-      btn.addEventListener('click', () => {
-        alert('送信されました');
-      });
+  
+  select.addEventListener('change', () => {
+    let value = document.form.job.value;
+    
+    if (value !== '無職') {
+      text.innerHTML = `${value}が選択されました`
     } else {
-      btn.disabled = true;
+      text.innerHTML = 'そんな時もあります'
     }
-  });
 
-
-  // ②
-  let check = document.getElementById('check');
-  const checkbox = document.form.check;
-  
-  const btn = document.getElementById('btn');
-  
-  let flag = true;
-  btn.addEventListener('click', () => {
-    if (check.checked) {
-      alert('同意が確認できました');
-
-      // 次に進むボタン作成
-      const input = document.createElement('input');
-      const button = document.createElement('button')
-      input.type = 'button';
-      input.value = '次に進む';
-      if (flag === true) {
-        document.body.appendChild(input);
-        flag = false;
-      };
-
-      input.addEventListener('click', () => {
-        if (check.checked) {
-          alert('次に進みます');
-        } else {
-          alert('同意されていません')
-        }
-      });
+    if (value !== '無職') {
+      text.classList.remove('class')
     } else {
-      alert('同意が確認できません');
+      text.classList.add('class');
     }
+    
+    
   });
-
 
 
 }
