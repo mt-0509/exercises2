f2ca9f0f4a5de867af11542f00f7fb2fa468b53f
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-15 23:37:44 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 'use strict';
 {
-  let select = document.getElementById('select');
-  const text = document.getElementById('text');
-  
-  
-  select.addEventListener('change', () => {
-    let value = document.form.job.value;
-    
-    if (value !== '無職') {
-      text.innerHTML = `${value}が選択されました`
-      text.classList.remove('class')
-    } else {
-      text.innerHTML = 'そんな時もあります'
-      text.classList.add('class');
-    }
-  });
+  const select = document.getElementById('select');
 
+  const p = document.getElementById('p')
+
+  // select.optionsの戻り値がHTMLOptionsCollectionなので配列に直す
+  // ...は個々の値に展開することができる
+  const chosenList = [...select.options];
+
+  select.addEventListener('change', () => {
+
+    // 選択肢が全て選択状態であればtrueを返す式
+    // 一つでも選択されていなければfalseを返す everyメソッド使用
+    // everyは与えられた関数の処理が全て通るかテストする関数
+    const result = chosenList.every(el => {
+      return el.selected;
+    });
+
+    if (result) {
+      return p.textContent = '太りますよ...'
+    }
+
+    // 選択状態を調べてselectedの場合のみ配列にpushする。reduceメソッド使用
+    // foeEachを使用の場合あらかじめ配列の用意が必要だが、
+    // reduceを使いselectされている物のみの配列を生成
+
+    // reduceの基本的な使い方は配列に格納されたデータをすべて合計した値を取得する、
+    const choices = chosenList.reduce((arr,cur)=>{
+      console.log(cur);
+    if(cur.selected){
+      arr.push(cur.value);
+    }
+    return arr;
+    },[]);
+
+    // joinは配列の全要素を連結した文字列を返す。また指定した文字列などで要素と要素を区切ることができる
+    p.textContent = choices.join('と');
+  })
 
 }
