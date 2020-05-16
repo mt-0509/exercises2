b97be8ff59836f45d468a08ba5c232db8c801218
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-16 21:34:45 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -2,15 +2,23 @@
 {
   const ans = document.getElementById('ans')
   const result = document.getElementById('result');
+  let min = 0;
+  let max = 10;
+  let rand = Math.floor(Math.random() * (max + 1 - min))+min;
+  console.log(rand);
 
   ans.addEventListener('blur', () => {
+
     let parse = parseInt(ans.value,10);
-    if (parse === 11) {
-      result.innerHTML = 'あたり';
+    result.classList.remove('none');
+
+    if (parse === rand) {
+      result.src = 'img/1.png';
     } else { 
-      result.innerHTML = 'はずれ';
+      result.src = 'img/2.jpeg';
     }
     ans.value = '';
+
   });
 
 
