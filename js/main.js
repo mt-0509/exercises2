eaf1e4a015913fdf4facd1784b4e156de3fe89a9
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-17 00:46:52 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 {
   const ans = document.getElementById('ans')
   const result = document.getElementById('result');
+  const mondai = document.getElementById('mondai');
+
   let min = 0;
   let max = 10;
-  let rand = Math.floor(Math.random() * (max + 1 - min))+min;
-  console.log(rand);
+  let rand1 = Math.floor(Math.random() * (max + 1 - min))+min;
+  let rand2 = Math.floor(Math.random() * (max + 1 - min))+min;
+
+  mondai.innerHTML = `${rand1} + ${rand2} = ?`
 
   ans.addEventListener('blur', () => {
 
     let parse = parseInt(ans.value,10);
     result.classList.remove('none');
 
-    if (parse === rand) {
+    if (parse === (rand1 + rand2)) {
       result.src = 'img/1.png';
     } else { 
       result.src = 'img/2.jpeg';
