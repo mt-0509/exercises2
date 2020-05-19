6ac782be7f6cd41df8aab7b18baeaeaeeab747e0
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-19 20:44:31 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -1,85 +1,25 @@
 'use strict';
 {
-  const ans = document.getElementById('ans');
-  const result = document.getElementById('result');
-  const mondaibun = document.getElementById('mondaibun');
-  const next = document.getElementById('next');
-  const onemore = document.getElementById('onemore');
-  const p = document.getElementById('p');
+  window.onload = () => {
+    // 現在の日付を取得する
+    let date1 = new Date();
+    let dnum1 = date1.getTime();
+    
+    // 未来の日付を取得する(年末)
+    let date2 = new Date(2020, 11, 31);
+    let dnum2 = date2.getTime();
+    
+  
+    // 未来と現在の差分を求める
+    let diffMsec = dnum2 - dnum1;
+    let diffDays = diffMsec / (1000 * 60 * 60 * 24);
+    let showDays = Math.ceil(diffDays);
 
-  // 問題を生成する
-  const mondai = () => {
-    let min = 0;
-    let max = 10;
-    let rand1;
-    let rand2;
+    // 表示する
+    let p = document.getElementById('p');
+    p.textContent = `あと ${showDays} 日`;
+  };
 
-    const x = () => {
-      rand1 = Math.floor(Math.random() * (max + 1 - min))+min;
-      rand2 = Math.floor(Math.random() * (max + 1 - min))+min;
-    
-      mondaibun.innerHTML = `${rand1} + ${rand2} = ?`
-    }
-    x();
-    let seikai = 0;
-  
-    ans.addEventListener('blur', () => {
-      if (isNumber(ans.value)) {
-        ans.disabled = true;
-    
-        let parse = parseInt(ans.value,10);
-        result.classList.remove('none');
-    
-        if (parse === (rand1 + rand2)) {
-          result.src = 'img/1.png';
-          next.classList.remove('none');
-          seikai++;
-          next.addEventListener('click', () => {
-            ans.disabled = false;
-            next.classList.add('none');
-            result.classList.add('none');
-            x();
-  
-          })
-    
-        } else { 
-          next.classList.add('none');
-          result.src = 'img/2.jpeg';
-          onemore.classList.remove('none');
-          seikai = 0;
-          onemore.addEventListener('click', () => {
-            ans.disabled = false;
-            x();
-            onemore.classList.add('none');
-            result.classList.add('none')
-          });
-  
-        }
-        ans.value = '';
-        if (seikai >= 3) {
-          result.src = 'img/3.png';
-          next.classList.add('none');
-          ans.classList.add('none');
-          p.classList.add('none');
-          mondaibun.classList.add('none');
-        }
-        
-      } else {
-        alert ('数字を入力してください')
-      }
-      
-  
-    });
-  }
-  mondai();
-
-  // 数字かどうかを判定する関数
-  function isNumber(numVal){
-    // チェック条件パターン
-    var pattern = /^[-]?([1-9]\d*|0)(\.\d+)?$/;
-    // 数値チェック
-    return pattern.test(numVal);
-  }
 
 
 
