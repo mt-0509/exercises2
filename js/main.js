dc9fe3775c227539d18f941c309c695e0fdaace9
mt-0509 <ppsr5504@yahoo.co.jp>
2020-05-20 20:47:24 +0900

    練習問題

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 'use strict';
 {
-  window.onload = () => {
-    // 現在の日付を取得する
-    let date1 = new Date();
-    let dnum1 = date1.getTime();
+  // id属性を取得
+  let start = document.getElementById('start');
+  let stop = document.getElementById('stop');
+  let p = document.getElementById('p');
+
+  // 開始ボタンが押された時間を取得する
+  let startTime = start.addEventListener('click', () => {
+    let startTime = new Date();
+    p.textContent = '';
+
+    // ボタンの表示・非表示切り替え
+    start.disabled = true;
+    stop.disabled = false;
     
-    // 未来の日付を取得する(年末)
-    let date2 = new Date(2020, 11, 31);
-    let dnum2 = date2.getTime();
-    
+    // 終了ボタンが押された時間を取得する
+    stop.addEventListener('click', () => {
+      let stopTime = new Date();
+      
+      // 差分を求める
+      let diffMsec = stopTime - startTime;
+      let diffSec = diffMsec / 1000;
+      let showSec = Math.round(diffSec);
+      p.textContent = `開始を押してから${showSec}秒後に終了しました`
+
+      // ループさせるため
+      start.disabled = false;
+      stop.disabled = true;
+      startTime = '';
+      stopTime = '';
+    });
+  });
+
   
-    // 未来と現在の差分を求める
-    let diffMsec = dnum2 - dnum1;
-    let diffDays = diffMsec / (1000 * 60 * 60 * 24);
-    let showDays = Math.ceil(diffDays);
-
-    // 表示する
-    let p = document.getElementById('p');
-    p.textContent = `あと ${showDays} 日`;
-  };
-
 
 
 
