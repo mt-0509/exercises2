7a1291c71731413d8a7684ae2c2f2dd93cd3fd41
mt-0509 <ppsr5504@yahoo.co.jp>
2020-06-02 22:19:50 +0900

    アロー関数について

--- js/index.js
+++ js/index.js
@@ -1,33 +1,21 @@
 'use strict';
 {
-  let map = new Map();
-  // ①要素の追加
-  map.set('one', 1);
-  map.set('two', 2);
-  map.set('three', 3);
+  // 定義方法
+  // (引数,...) => {...関数の本体...}
 
-  // ②要素数の取得
-  console.log(map.size);
+  let double = (num) => {
+    return num * 2
+  };
+  console.log(double(9));
 
-  // ③指定したキーの取得
-  console.log(map.get('two'));
+  // 省略記法
+  let triple = num => num * 3;
+  console.log(triple(4));
 
-  // ④指定したキーの要素が存在するか
-  console.log(map.has('one'));
-  console.log(map.has('four'));
-
-  // ⑤指定したキーの要素を削除
-  map.delete('three')
-  console.log(map.size);
-
-  map.forEach(function(value, key) {
-    console.log("[" + key + ", " + value + "]")
-  })
-
-  // Mapの利点
-  // キーに任意の型を使うことができる
-  // サイズプロパティを使って簡単に要素の数を取得することができる
-  // 完全にからの連想配列を作成することができる
+  // functionとアロー関数の違い
+  // ①thisの指す場所が違う
+  // ②アロー関数はコンストラクタになることができないのでextendsできない
+  // などなど
 
 
 }
